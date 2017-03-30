dafb5c36bdb70afa6e7614d59cade7500235878e
Jake Headley <jheadley@roomkey.com>
2017-03-30 13:53:04 -0600

    Initial set up of app for drill 5

---
+++ app5/controller.js
@@ -0,0 +1,12 @@
+angular.module('myApp5').controller('mainCtrl', function($scope){
+
+  // Test to make sure app is working:
+  $scope.test = "bOOya!! The app is working.";
+
+  // apiService.getPokemon().then(function(responce){
+  //   $scope.pokemon = responce;
+  // });
+
+
+
+});
